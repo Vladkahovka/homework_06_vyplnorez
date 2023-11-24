348fabcd89374a93a33d04fb17f6188c53fc0b1d
Vladka Vrana <hovka@seznam.cz>
2023-11-24 22:46:25 +0100

    Ukol c. 06

---
+++ script.js
@@ -0,0 +1,36 @@
+/* Zadání
+Napište funkci fillcut, která jako svůj první parametr str 
+očekává řetězec a jako druhý parametr len kladné celé číslo. 
+Úkolem funkce je oříznout nebo prodloužit zadaný řetězec tak, 
+aby měl délku přesně len.
+Pokud je vstupní řetězec delší než len, tak funkce odřízne 
+jeho konec a vrátí výsledek.
+Pokud je vstupní řetězec kratší než len, tak jej doplní od 
+začátku znakem tečky a vrátí výsledek.
+Pokud je vstupní řetězec dlouhý přesně len, funkce jej vrátí 
+beze změny.
+Příklad použití:
+
+document.body.innerHTML += fillcut('petr', 8) + '<br>'; // vypíše „....petr“
+document.body.innerHTML += fillcut('petr', 3) + '<br>'; // vypíše „pet“
+document.body.innerHTML += fillcut('petr', 4) + '<br>'; // vypíše „petr“ */
+
+function fillcut(str, len) {
+     if (str.length > len) {
+         return str.slice(0, len)
+     } else if (str.length < len) {
+          return ".".padStart(4, ".", len - str.length) + str 
+          } else {
+         return str
+     }
+ }
+
+ const str = "Vladka"
+ const len = 6
+ const vysledek = fillcut(str, len)
+
+ document.body.innerHTML += `
+ <p> ${fillcut("Vladka", 10) } </p>
+ <p> ${fillcut("Vladka", 3) } </p>
+ <p> ${fillcut("Vladka", 6) } </p>
+ `
